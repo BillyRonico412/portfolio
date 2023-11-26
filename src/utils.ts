00299fc677b67f2cde890ee78d9180602f4abaa3
RonicoBillly <billyronico412@gmail.com>
2023-11-26 17:25:34 +0100

    remove webdevdaily

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,17 @@ export const sortByDate = (
 	a: CollectionEntry<"project">,
 	b: CollectionEntry<"project">,
 ) => {
-	const dateB = new Date()
-	dateB.setMonth(b.data.month - 1)
-	dateB.setFullYear(b.data.year)
-	const dateA = new Date()
-	dateA.setMonth(a.data.month - 1)
-	dateA.setFullYear(a.data.year)
-	return dateB.getTime() - dateA.getTime()
+	if (a.data.year > b.data.year) {
+		return -1
+	}
+	if (a.data.year < b.data.year) {
+		return 1
+	}
+	if (a.data.month > b.data.month) {
+		return -1
+	}
+	if (a.data.month < b.data.month) {
+		return 1
+	}
+	return 1
 }
